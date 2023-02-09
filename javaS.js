22ee8889c1a48a6640f0bd8a94132e6e339061e2
1Biv <123101648+1Biv@users.noreply.github.com>
2023-02-09 11:26:23 +0100

    haciendo el login y el signup
    tengo que arreglar el login y hacer el signup

--- javaS.js
+++ javaS.js
@@ -1,9 +1,11 @@
 function openNav() {
-    document.getElementById("menuVertical").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("menuVertical").style.width = "500px";
+    document.getElementById("main").style.marginLeft = "500px";
 }
 function closeNav() {
     document.getElementById("menuVertical").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+var modal=document.getElementById('entrar');
+
 
